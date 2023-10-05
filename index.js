be1285a7f74bccab1f39a1ddfcfa28a90eb5b760
gavriel <gbatyrov10@gmail.com>
2023-10-05 11:51:09 -0400

    test

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 function openMenu() {
-    document.body.classList += " menu--open"
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu--open')
-  }
- 
-  function disableScroll(){
-    var top = $(window).scrollTop();
-    var left = $(window).scrollLeft();
-    $('body').css('overflow', 'hidden');
-    $(window).scroll(function(){
-        $(this).scrollTop(top).scrollLeft(left);
-    });
+  document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
 }
